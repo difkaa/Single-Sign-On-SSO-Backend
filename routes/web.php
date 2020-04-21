7b0f2551adaa83d409a85b0a4bbf54d1a5363218
difka09 <adifka09@gmail.com>
2020-04-21 08:01:35 +0700

    default authorize

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/settings', 'SettingsController@index')->name('settings');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
